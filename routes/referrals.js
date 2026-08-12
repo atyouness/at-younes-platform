e54d47b944af79d89a7aa750cf4cfab4ed47881b
atyouness <bouabboune@gmail.com>
2026-08-12 23:36:32 +0100

    Add referral route with temporary message

--- routes/referrals.js
+++ routes/referrals.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.json({ message: '✅ مسار الإحالات يعمل (مؤقت)' });
+});
+
+module.exports = router;
