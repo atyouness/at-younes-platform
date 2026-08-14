fa6efa4ab4ee9bd37cb9ef64fa345a1a5a5aa7f3
atyouness <bouabboune@gmail.com>
2026-08-14 07:34:18 +0100

    Change database host from 'localhost' to '127.0.0.1'

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || '127.0.0.1',
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'u188242994_atyouness',
   password: process.env.DB_PASSWORD || 'UJN741ik85/*',
